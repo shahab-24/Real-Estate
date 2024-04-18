17bd7ac7b88bdd34588ff77faec05a1a0d1cb86e
Shahab Uddin <shahab.82946@gmail.com>
2024-04-18 22:36:50 +0600

    edit reg

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -66,7 +66,7 @@ const Register = () => {
         </div>
         {error && <div className="text-red-500">{error}</div>}
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Register</button>
+          <button className="btn btn-primary text-xl">Register</button>
           
         </div>
         <div className='flex items-center justify-between'>
